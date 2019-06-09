546f606eac23bee40c2ba87935eb1f9ef9132944
Adam Ugas <adenugaas@gmail.com>
2019-06-09 17:05:36 -0500

    final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,8 @@
-//importing mysql from the package
+
 var mysql = require("mysql");
 var inquirer = require('inquirer');
 
 
-// This are object variable which are used for connecting to mySQl database
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,7 +11,7 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-// here we will create call back function to confirm if connection was successful:
+
 
 connection.connect(function (err) {
     if (err) throw (err);
