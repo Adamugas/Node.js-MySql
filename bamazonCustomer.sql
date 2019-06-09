e6b81b56f17450431ca35d668f148d18ca0134bc
Adam Ugas <adenugaas@gmail.com>
2019-06-09 17:03:39 -0500

    final

--- bamazonCustomer.sql
+++ bamazonCustomer.sql
@@ -4,31 +4,31 @@ USE bamazon_db;
 
 CREATE Table products
 (
-    itemID INT (15)
-    auto_increment NOT NULL,
+  itemID INT (15)
+  auto_increment NOT NULL,
 product_name  varchar
-    (200) NOT NULL,
+  (200) NOT NULL,
 department_name varchar
-    (50) NOT NULL,
+  (50) NOT NULL,
 price INT
-    (15) NOT NULL,
+  (15) NOT NULL,
 stock_quantity INT
-    (14) NOT NULL,
+  (14) NOT NULL,
 PRIMARY KEY
-    (itemID)
+  (itemID)
 );
-    INSERT INTO products
-        (product_name , department_name, price,stock_quantity)
-    VALUES('iphone', 'electronics', 750, 50),
-        ('earphones', 'electronics', 45, 30),
-        ('doorhandle', 'tools', 30, 100),
-        ('baby_cereal', 'food', 20, 200),
-        ('banana', 'food', 20, 150),
-        ('fruits', 'food', 60, 150),
-        ('rice', 'food', 30, 29),
-        ('remotes', 'electronic', 10, 34),
-        ('ovens', 'kitchen', 15, 35),
-        ('milkbottle', 'utensil', 34, 40);
-    SELECT*
-    FROM products;
+  INSERT INTO products
+      (product_name , department_name, price,stock_quantity)
+  VALUES('soccerballs', 'sports', 300, 20),
+      (' men shoes', 'shoe', 45, 30),
+      ('women shoes', 'shoe', 30, 100),
+      ('cereal', 'food', 20, 200),
+      ('banana', 'food', 20, 150),
+      ('fruits', 'food', 60, 150),
+      ('rice', 'food', 30, 29),
+      ('remotes', 'electronic', 10, 38),
+      ('ovens', 'kitchen', 15, 40),
+      ('milkbottle', 'utensil', 34, 40);
+  SELECT*
+  FROM products;
        
